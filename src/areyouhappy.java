68f58475454e04f7803c8c3c80cc49452b2befe4
league <league@192.168.7.109>
2018-08-11 14:28:22 -0700

    are you happy

--- src/areyouhappy.java
+++ src/areyouhappy.java
@@ -1,4 +1,11 @@
+import javax.swing.JOptionPane;
 
 public class areyouhappy {
-
+	public static void main(String[] args) {
+String happy = JOptionPane.showInputDialog("Are you happy?");
+if(happy.equals("yes")) {
+JOptionPane.showMessageDialog(null, "Keep doing what you're doing");
 }
+}
+	}
+
